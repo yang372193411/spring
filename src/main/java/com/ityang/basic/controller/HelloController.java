450585e07e3b8f0a314d835dfb87bf98fb32fca1
yang372193411 <yang372193411@163.com>
2018-07-22 00:20:15 +0800

    各种配置

--- src/main/java/com/ityang/basic/controller/HelloController.java
+++ src/main/java/com/ityang/basic/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.ityang.basic.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by Administrator on 2018/7/21.
  */
 
 @RestController
+@RequestMapping("hello")
+@Api("swaggerDemoController相关的api")
 public class HelloController {
 
     @RequestMapping("hello")
@@ -17,5 +24,21 @@ public class HelloController {
     @RequestMapping("test")
     public String Test(){
         return  "test";
+    }
+
+    @RequestMapping("yang")
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
+    public String toString(Integer id) {
+        String sb1 = null;
+        try {
+            byte[] bytes = "你好".getBytes();//"UTF-16");
+            System.out.println(bytes.toString());
+            sb1 = new String(bytes, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sb1);
+        return sb1;
     }
 }
