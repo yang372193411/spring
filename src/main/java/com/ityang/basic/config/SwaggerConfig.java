88ba4d5ba823cb331fae46993cd5e3ad36f61a64
yang372193411 <yang372193411@163.com>
2019-09-04 17:32:31 +0800

    Swagger提示

--- src/main/java/com/ityang/basic/config/SwaggerConfig.java
+++ src/main/java/com/ityang/basic/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.ityang.basic.config;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,11 +15,15 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Yang的API").description("API").termsOfServiceUrl("#").version("1.0").build();
     }
 }
